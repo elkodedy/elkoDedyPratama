37a785da5318e82ae33157f539a2e9ee2fd9dbdb
elkodedy <37838989+elkodedy@users.noreply.github.com>
2018-05-28 13:15:15 +0800

    Add files via upload

---
+++ ElkoDedyPratama.java
@@ -0,0 +1,22 @@
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package elkodedypratama;
+
+/**
+ *
+ * @author Elko
+ */
+public class ElkoDedyPratama {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        for(int i=1;i<=10;i++)
+            System.out.println( i + " * 8 = " + (i*8));
+    }
+}
